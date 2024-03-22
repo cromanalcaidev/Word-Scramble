ad003133b349a8f6392428454c3c63cfe7648439
Carlos Román Alcaide <cromanalcaidev@gmail.com>
2024-03-22 16:48:34 +0100

    Initial Commit

---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Carlos Román Alcaide on 22/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
